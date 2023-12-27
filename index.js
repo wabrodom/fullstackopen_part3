26708070672408c4e01d57dcc148f1d470a61ff2
wabrodom <113647599+wabrodom@users.noreply.github.com>
2023-12-27 18:47:50 +0700

    do exercises 3.7-3.8 use morgan and custom format string with token

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require('morgan')
 const app = express();
 
 app.use(express.json());
+
+morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
   {
