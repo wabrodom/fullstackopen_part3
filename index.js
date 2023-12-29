1e2736d7edf59574e78b85964e6c84be8a0fef38
wabrodom <113647599+wabrodom@users.noreply.github.com>
2023-12-29 20:42:03 +0700

    add npm script to handle new production build of frontend and deploy script, add ignore dist directory

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.use(cors())
 app.use(express.json());
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+app.use(express.static('dist'))
 
 let persons = [
   {
